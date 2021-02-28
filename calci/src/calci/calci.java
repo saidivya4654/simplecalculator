47d8edbbce627a93bb7ab664668fd51114520fab
saidivya4654 <saidivya4654@gmail.com>
2021-02-28 20:17:53 +0600

    commit

--- calci/src/calci/calci.java
+++ calci/src/calci/calci.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class Calculator {
 public static void main(String[] args)
 {
 	Scanner s=new Scanner(System.in);
+	Calculator cal=new Calculator();
 	System.out.print("Enter two numbers");
-	double a=s.nextDouble();
-	double b=s.nextDouble();
+	final int a=s.nextInt();
+	final int b=s.nextInt();
 	double c=0;
 	char s1=s.next().charAt(0);
 	switch(s1)
 	{
-	case '+':c=a+b;
+	case '+':c=(double)+(double)b;
 	         break;
-	case '-':c=a-b;
+	case '-':c=(double)a-(double)b;
 	         break;
-	case '*':c=a*b;
+	case '*':c=(double)a*(double)b;
 	         break;
-	case '/':c=a/b;
+	case '/':c=(double)a/(double)b;
 	         break;
 	default:System.out.println("Enter the correct operator");
 	
